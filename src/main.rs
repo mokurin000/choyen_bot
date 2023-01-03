91e4babf4b33c3220d7a495739ac0c04b5b5bc79
poly000 <1348292515@qq.com>
2023-01-03 16:34:42 +0800

    fix command mode thanks to @Itsusinn

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use teloxide::{
     dispatching::DpHandlerDescription,
     prelude::*,
     types::{
-        InlineQuery, InlineQueryResult, InlineQueryResultArticle, InlineQueryResultCachedPhoto,
-        InputMessageContent, InputMessageContentText,
+        InlineQuery, InlineQueryResult, InlineQueryResultArticle,
+        InputMessageContent, InputMessageContentText, InlineQueryResultCachedSticker,
     },
     types::{InputFile, MediaKind, MessageKind},
     utils::command::BotCommands,
@@ -29,11 +29,16 @@ async fn main() {
     let inline_handler = inline_handler();
     let command_handler = command_handler();
 
-    Dispatcher::builder(bot, inline_handler.branch(command_handler))
-        .enable_ctrlc_handler()
-        .build()
-        .dispatch()
-        .await;
+    Dispatcher::builder(
+        bot,
+        dptree::entry()
+            .branch(inline_handler)
+            .branch(command_handler),
+    )
+    .enable_ctrlc_handler()
+    .build()
+    .dispatch()
+    .await;
 }
 
 #[derive(BotCommands, Clone)]
@@ -69,7 +74,7 @@ fn inline_handler() -> teloxide::prelude::Handler<
             let input_photo = InputFile::file(file);
 
             let upload_photo = bot
-                .send_photo(PRAVITE_CHANNEL_ID.to_owned(), input_photo)
+                .send_sticker(PRAVITE_CHANNEL_ID.to_owned(), input_photo)
                 .send()
                 .await;
             match upload_photo {
@@ -77,10 +82,10 @@ fn inline_handler() -> teloxide::prelude::Handler<
                     let mut results = Vec::new();
                     let kind = resp.kind;
                     if let MessageKind::Common(common) = kind {
-                        if let MediaKind::Photo(photo) = common.media_kind {
-                            let file_id = &photo.photo[0].file.id;
-                            let cached_photo = InlineQueryResultCachedPhoto::new("0", file_id);
-                            results.push(InlineQueryResult::CachedPhoto(cached_photo));
+                        if let MediaKind::Sticker(sticker) = common.media_kind {
+                            let file_id = &sticker.sticker.file.id;
+                            let cached_sticker = InlineQueryResultCachedSticker::new("0", file_id);
+                            results.push(InlineQueryResult::CachedSticker(cached_sticker));
                         }
                     }
                     results
@@ -113,35 +118,29 @@ fn command_handler() -> teloxide::prelude::Handler<
     Result<(), RequestError>,
     DpHandlerDescription,
 > {
-    Update::filter_message()
-        .filter_command::<Command>()
-        .branch(dptree::endpoint(
-            |bot: Bot, message: Message, command: Command| async move {
-                let response = match command {
-                    Command::Help => {
-                        bot.send_message(message.chat.id, Command::descriptions().to_string())
-                            .await
-                    }
-                    Command::Choyen(text) => {
-                        let unique_id = message.id.0;
-                        let file = PathBuf::from(&format!("temp/{unique_id}.webp"));
+    Update::filter_message().branch(dptree::entry().filter_command::<Command>().endpoint(answer))
+}
 
-                        if let Some((top, bottom)) = text.split_once("|") {
-                            generate_5000choyen(top, bottom, &file).unwrap();
-                            let input_photo = InputFile::file(file);
-                            bot.send_animation(message.chat.id, input_photo).await
-                        } else {
-                            bot.send_message(message.chat.id, "usage:\n/choyen [top]|[bottom]")
-                                .await
-                        }
-                    }
-                };
+async fn answer(bot: Bot, message: Message, command: Command) -> ResponseResult<()> {
+    match command {
+        Command::Help => {
+            bot.send_message(message.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Choyen(text) => {
+            let unique_id = message.id.0;
+            let file = PathBuf::from(&format!("temp/{unique_id}.webp"));
 
-                if let Err(e) = response {
-                    log::error!("Error in command: {e}");
-                }
+            if let Some((top, bottom)) = text.split_once("|") {
+                generate_5000choyen(top, bottom, &file).unwrap();
+                let input_photo = InputFile::file(file);
+                bot.send_animation(message.chat.id, input_photo).await?
+            } else {
+                bot.send_message(message.chat.id, "usage:\n/choyen [top]|[bottom]")
+                    .await?
+            }
+        }
+    };
 
-                respond(())
-            },
-        ))
+    Ok(())
 }
